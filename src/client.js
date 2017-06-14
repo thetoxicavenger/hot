62d0dac0446ac58402c5afe42135148a551373e7
Mitchell Cravens <mitch@fjld.co>
2017-06-13 21:33:52 -0700

    good start.

--- src/client.js
+++
@@ -1,3 +0,0 @@
-// this should be done automatically too
-import {foo} from './routes/index/client/index.js'
-console.log(foo, 'hi')
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,25 +1,10 @@
 const {join} = require('path')
+const express = require('express')
 module.exports = (app) => {
-
-  app.set('views', join(__dirname, '..', 'routes'));
-  app.set('view engine', 'hbs');
 
   if (process.env.NODE_ENV === 'dev') {
     app.use(require('morgan')('short'));
-    (function() { // why the closure?
-      // Step 1: Create & configure a webpack compiler
-      var webpack = require('webpack');
-      var webpackConfig = require(join(__dirname, '..', '..', 'webpack.config'));
-      var compiler = webpack(webpackConfig);
-      // Step 2: Attach the dev middleware to the compiler & the server
-      app.use(require("webpack-dev-middleware")(compiler, {
-        noInfo: true, publicPath: webpackConfig.output.publicPath
-      }));
-      // Step 3: Attach the hot middleware to the compiler & the server
-      app.use(require("webpack-hot-middleware")(compiler, {
-        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-      }));
-    })();
+    app.use(express.static(join(__dirname, '..', 'static')))
     return app
   }
 }
